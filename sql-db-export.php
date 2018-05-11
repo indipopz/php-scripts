e8ecc8d48ebc52cfec222a997e6a848b8e205fe4
indipopz <nitish.kr.mallick@gmail.com>
2018-05-11 13:02:12 +0530

    Update sql-db-export.php

--- sql-db-export.php
+++ sql-db-export.php
@@ -1,5 +1,5 @@
          
-```php ini_set("memory_limit", "-1");
+         ini_set("memory_limit", "-1");
            set_time_limit(0);
            $host = $model->hostAddress;
            $user = $model->dbUser;
